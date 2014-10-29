9661e1d7b7ebcf6d445efb3408014c2d3ec6c9be
Maxime Tricoire <max.tricoire@gmail.com>
2014-10-29 18:14:54 +0100

    alright: first impl

---
+++ index.js
@@ -0,0 +1,2 @@
+module.exports = require('./lib/Client');
+module.exports.Server = require('./lib/Server');
---
+++ test/test_client.js
@@ -0,0 +1,46 @@
+// Created by leiko on 29/10/14 14:58
+var expect = require('chai').expect;
+
+var WSMsgBroker = require('../index');
+
+describe('WSMsgBroker tests', function () {
+    var server = new WSMsgBroker.Server(9050);
+    var client0, client1;
+
+    it('should create and register a new WSMsgBroker client "client0" on the server', function (done) {
+        client0 = new WSMsgBroker('client0', 'localhost', 9050);
+        client0.on('registered', function () {
+            done();
+        });
+    });
+
+    it('should create and register a new WSMsgBroker client "client1" on the server', function (done) {
+        client1 = new WSMsgBroker('client1', 'localhost', 9050);
+        client1.on('registered', function () {
+            done();
+        });
+    });
+
+    it('should send "foo" to "client1" and get a "bar" answer', function (done) {
+        client1.on('message', function fooHandler(msg, sender) {
+            sender.send('bar');
+            client1.off('message', fooHandler);
+        });
+
+        client0.send('foo', 'client1', function (from, answer) {
+            expect(from).to.equal('client1');
+            expect(answer).to.equal('bar');
+            done();
+        });
+    });
+
+    it('should send "potato" to "client1" and "wrong" (but wrong is not registered yet so does nothing)', function (done) {
+        client1.on('message', function (msg) {
+            expect(msg).to.equal('potato');
+            done();
+        });
+
+        client0.send('potato', ['client1', 'wrong']);
+    });
+});
+
